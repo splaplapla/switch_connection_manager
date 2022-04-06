9b3de8f42bee7cea3c826c6a0939e186f111b556
jiikko <n905i.1214@gmail.com>
2022-04-06 22:56:56 +0900

    first commit

---
+++ lib/switch_connection_manager.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "switch_connection_manager/version"
+
+module SwitchConnectionManager
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/switch_connection_manager/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SwitchConnectionManager
+  VERSION = "0.1.0"
+end
