a4a6db29451fc99b7a9ca810fa438ce5eb639109
jiikko <n905i.1214@gmail.com>
2022-04-10 03:08:10 +0900

    pry

--- exe/procon.rb
+++ exe/procon.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'bundler/inline'
 require "bundler/setup"
 require "switch_connection_manager"
+
+gemfile do
+  source 'https://rubygems.org'
+  git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+  gem 'pry'
+end
 
 SwitchConnectionManager::Procon.new.run
