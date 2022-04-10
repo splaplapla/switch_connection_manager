80df58bc0a010d5d2d7c2b9854a05aaafa52113b
jiikko <n905i.1214@gmail.com>
2022-04-10 15:51:41 +0900

    a

--- lib/switch_connection_manager/procon.rb
+++ lib/switch_connection_manager/procon.rb
@@ -69,6 +69,10 @@ class SwitchConnectionManager::Procon
     at_exit do
       if procon
         send_to_procon("0000")
+        send_to_procon("0000")
+        send_to_procon("8005")
+        send_to_procon("0000")
+        send_to_procon("8001")
         procon.close
       end
     end
