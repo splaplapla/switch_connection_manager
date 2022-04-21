3d65d5e04264bd9468e731a88d007b452c1b7219
jiikko <n905i.1214@gmail.com>
2022-04-21 23:10:10 +0900

    a

--- lib/switch_connection_manager/mouse.rb
+++ lib/switch_connection_manager/mouse.rb
@@ -1,20 +1,51 @@
 class SwitchConnectionManager::Mouse
-  def run
-    loop do
+  def Device
+    def read
       blocking_read
+    end
+
+    def blocking_read
+      raw_data = mouse.read(16)
+      to_stdout("<<< #{raw_data.unpack("H*").first}")
+      return raw_data
+    end
+
+    private
+
+    def non_blocking_read
+      raw_data = mouse.read_nonblock(32)
+      to_stdout("<<< #{raw_data.unpack("H*").first}")
+      return raw_data
+    rescue IO::EAGAINWaitReadable
+      sleep(0.05)
+    end
+
+    def mouse
+      path = SwitchConnectionManager::MouseFinder.find or raise("device could not find")
+      @mouse ||= File.open(path, "w+b")
+    end
+
+    def to_stdout
+      puts(text)
     end
   end
 
-  private
+  class DataParser < Struct.new(:tv_sec, :tv_usec, :type, :code, :value)
+    def self.parse(raw_data)
+      new.parse(raw_data)
+    end
 
-  def blocking_read
-    raw_data = mouse.read(64)
-    to_stdout("<<< #{raw_data.unpack("H*").first}")
-    return raw_data
+    def parse(data)
+      DataParser.new(*data.unpack("llSSl"))
+    end
   end
 
-  def mouse
-    path = SwitchConnectionManager::MouseFinder.find or raise("device could not find")
-    @mouse ||= File.open(path, "w+b")
+  def run
+    device = Device.new
+
+    loop do
+      raw_data = device.read
+      DataParser.parse(raw_data)
+    end
   end
 end
