1a92edcd902305235e36316d9e18ca17ad6d739c
jiikko <n905i.1214@gmail.com>
2023-12-16 20:45:31 +0900

    log

--- lib/switch_connection_manager.rb
+++ lib/switch_connection_manager.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
-require "switch_connection_manager/version"
-require "switch_connection_manager/procon_simulator"
-require "switch_connection_manager/procon_simulator2"
-require "switch_connection_manager/procon"
-require "switch_connection_manager/old_procon"
-require "switch_connection_manager/mouse"
-require "switch_connection_manager/procon/procon_connection_status"
-require "switch_connection_manager/procon/procon_internal_status"
-require "switch_connection_manager/support/mouse_finder"
-require "switch_connection_manager/support/procon_finder"
-require "switch_connection_manager/support/usb_device_controller"
+require 'logger'
 
 module SwitchConnectionManager
   def self.logger
@@ -21,3 +11,15 @@ module SwitchConnectionManager
     @logger = logger
   end
 end
+
+require 'switch_connection_manager/version'
+require 'switch_connection_manager/procon_simulator'
+require 'switch_connection_manager/procon_simulator2'
+require 'switch_connection_manager/procon'
+require 'switch_connection_manager/old_procon'
+require 'switch_connection_manager/mouse'
+require 'switch_connection_manager/procon/procon_connection_status'
+require 'switch_connection_manager/procon/procon_internal_status'
+require 'switch_connection_manager/support/mouse_finder'
+require 'switch_connection_manager/support/procon_finder'
+require 'switch_connection_manager/support/usb_device_controller'
